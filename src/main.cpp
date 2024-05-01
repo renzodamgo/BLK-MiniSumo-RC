dacea73764ce72e4032b7f29fed656e046175643
Renzo Damian <renzo.damian.go@gmail.com>
2024-05-01 14:57:17 -0500

    Format c++

--- src/main.cpp
+++ src/main.cpp
@@ -15,48 +15,44 @@
 Tb6612fng motors(STANDBY, AIN1_LEFT, AIN2_LEFT, PWMA_LEFT, BIN1_RIGHT,
                  BIN2_RIGHT, PWMB_RIGHT);
 
+bool connected = false;
+
 void setup() {
-  Serial.begin(115200);
-  motors.begin();
-  ps5.begin(DUALSENSE_MAC);
-  Serial.println("Ready.");
+    Serial.begin(115200);
+    motors.begin();
+    ps5.begin(DUALSENSE_MAC);
+    Serial.println("Ready.");
 }
 
 void loop() {
-
-  // Serial.printf("Loop: %d\n", millis());
-  // // 500ms forwards
-  // motors.drive(0.5, 500);
-  // // 500ms backwards
-  // motors.drive(-0.5, 500);
-  // // Full range of motor speed
-  // for (auto i = 1; i <= 10; i += 1) {
-  //   motors.drive(0.1 * i, 200, false);
-  // }
-  // motors.brake();
-  // delay(1000)
-  while (ps5.isConnected()) {
-    Serial.println("Connected!");
-    // Serial.printf("Loop: %d\n", millis());
-    float left_pwm = 0.0;                  // 0..1
-    float right_pwm = 0.0;                 // 0..1
-    float speed_normalized = 0.0;          // -1.00 ..  1.00
-    if (ps5.LStickX() < -STICK_DEADZONE) { // x-Left from -30 to -1
-      
+    while (ps5.isConnected()) {
+        if (!connected) {
+            connected = true;
+            Serial.println("Connected!");
+        }
+        Serial.println("Connected!");
+        float left_pwm = 0.0;                   // 0..1
+        float right_pwm = 0.0;                  // 0..1
+        float speed_normalized = 0.0;           // -1.00 ..  1.00
+        if (ps5.LStickX() < -STICK_DEADZONE) {  // x-Left from -30 to -1
+            Serial.printf("LStickX: %d\n", ps5.LStickX());
+        }
+        if (STICK_DEADZONE < ps5.LStickX()) {  // x-right from 30 to 1
+            Serial.printf("LStickX: %d\n", ps5.LStickX());
+        }
+        if (ps5.L2()) {
+            Serial.printf("L2 button at %d\n", ps5.L2Value());
+        }
+        if (ps5.R2() || ps5.L2()) {  // 0..255 Acelerar
+            float accelerate_normalized =
+                float(map(ps5.R2Value(), 0, 255, 0, 100)) / 100.0;
+            float retro_normalized =
+                float(map(ps5.L2Value(), 0, 255, 0, 100)) / 100.0;
+            speed_normalized = accelerate_normalized - retro_normalized;
+            Serial.printf("speed_normalized: %.2f\n", speed_normalized);
+            Serial.printf("R2 button at %d\n", ps5.R2Value());
+            motors.drive(speed_normalized, speed_normalized, 30);
+        }
+        delay(10);
     }
-    if (STICK_DEADZONE < ps5.LStickX()) { // x-right from 30 to 1
-    
-    }
-    if (ps5.R2() || ps5.L2()) { // 0..255 Acelerar
-      float accelerate_normalized =
-          float(map(ps5.R2Value(), 0, 255, 0, 100)) / 100.0;
-      float retro_normalized =
-          float(map(ps5.L2Value(), 0, 255, 0, 100)) / 100.0;
-      speed_normalized = accelerate_normalized - retro_normalized;
-      Serial.printf("speed_normalized: %.2f\n", speed_normalized);
-      Serial.printf("R2 button at %d\n", ps5.R2Value());
-      motors.drive(speed_normalized, speed_normalized, 30);
-    }
-    delay(10);
-  }
 }

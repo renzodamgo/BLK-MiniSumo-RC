10734afb9227c3eef26fffe53add81a53612ff7f
Renzo Damian <renzo.damian.go@gmail.com>
2024-06-05 18:07:42 -0500

    Add dance moves and rotation with r1 and l1

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,7 @@
 #define DUALSENSE_MAC "D0:BC:C1:05:69:31"
 #define STICK_DEADZONE 30
 
-Tb6612fng motors(STANDBY, AIN1_LEFT, AIN2_LEFT, PWMA_LEFT, BIN1_RIGHT,
-                 BIN2_RIGHT, PWMB_RIGHT);
+Tb6612fng motors(STANDBY, BIN1_RIGHT, BIN2_RIGHT, PWMB_RIGHT, AIN1_LEFT, AIN2_LEFT, PWMA_LEFT);
 
 bool connected = false;
 
@@ -22,6 +21,27 @@ void setup() {
     motors.begin();
     ps5.begin(DUALSENSE_MAC);
     Serial.println("Ready.");
+}
+
+void dance() {
+    for (int i = 0; i < 4; i++) {
+        motors.drive(0.5, -0.5, 30);  // Turn right
+        delay(100);
+        motors.drive(-0.5, 0.5, 30);  // Turn left
+        delay(100);
+        motors.drive(0.5, -0.5, 30);  // Turn right
+        delay(100);
+        motors.drive(-0.5, 0.5, 30);  // Turn left
+        delay(100);
+        motors.drive(-0.5, -0.5, 30);  // Turn right
+        delay(100);
+        motors.drive(0.5, 0.5, 30);  // Turn left
+        delay(100);
+        motors.drive(-0.5, -0.5, 30);  // Turn right
+        delay(100);
+        motors.drive(0.5, 0.5, 30);  // Turn left
+        delay(100);
+    }
 }
 
 void loop() {
@@ -34,7 +54,7 @@ void loop() {
         float left_pwm = 1.0;                   // 0..1
         float right_pwm = 1.0;                  // 0..1
         float speed_normalized = 0.0;           // -1.00 ..  1.00
-        if (ps5.LStickX() < -STICK_DEADZONE) {  // x-Left from -30 to -1
+        if (ps5.LStickX() < -STICK_DEADZONE) {  // x-Left from -30 to -1pa
             // Serial.printf("LStickX: %d\n", ps5.LStickX());
             left_pwm = float(map(ps5.LStickX(), -140, 0, 0, 100)) / 100.0;
             Serial.printf("left_pwm: %.2f\n", left_pwm);
@@ -44,6 +64,25 @@ void loop() {
             right_pwm = float(map(ps5.LStickX(), 0, 130, 100, 0)) / 100.0;
             Serial.printf("right_pwm: %.2f\n", right_pwm);
         }
+
+        if (ps5.L1()) {
+            right_pwm = 0.5;
+            left_pwm = -0.5;
+            motors.drive(left_pwm, right_pwm, 30);
+            Serial.println("R1 button pressed");
+        }
+
+        if (ps5.R1()) {
+            left_pwm = 0.5;
+            right_pwm = -0.5;
+            motors.drive(left_pwm, right_pwm, 30);
+            Serial.println("R1 button pressed");
+        }
+
+        if (ps5.R3()) {
+            dance();
+        }
+
         if (ps5.R2() || ps5.L2()) {  // 0..255 Acelerar
             float accelerate_normalized =
                 float(map(ps5.R2Value(), 0, 255, 0, 100)) / 100.0;
@@ -52,7 +91,7 @@ void loop() {
             speed_normalized = accelerate_normalized - retro_normalized;
             // Serial.printf("speed_normalized: %.2f\n", speed_normalized);
             // Serial.printf("R2 button at %d\n", ps5.R2Value());
-            motors.drive(left_pwm * speed_normalized, right_pwm * speed_normalized, 30);
+            motors.drive(left_pwm * speed_normalized, right_pwm * speed_normalized, 20);
             Serial.printf("left_pwm: %.2f\n", left_pwm * speed_normalized);
             Serial.printf("right_pwm: %.2f\n", right_pwm * speed_normalized);
         }
